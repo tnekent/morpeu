631ef8a85ab1b8a67e0ab800502ae2501442f823
Kent Ahon <kentahon04@gmail.com>
2020-05-04 13:37:14 +0800

    Add test to index.test.ts

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,3 +4,10 @@ test("errors when no arguments given", () => {
     // Simulate JavaScript behavior by passing undefined
     expect(() => morph(undefined)).toThrow();
 });
+
+test("can construct Markdown heading + link", () => {
+    const sturcture = { name: "Bar", link: "bar.com" },
+        result = morph("## [{.name}]({0.link})", sturcture);
+
+    expect(result).toBe("## [Bar](bar.com)");
+});
